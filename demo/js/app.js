1b1c24c60b69fe834c3b57d384e640d77f765821
Kite#06 <109735090+kiet06nguyen@users.noreply.github.com>
2023-12-06 17:29:04 +0700

    Update app.js

--- demo/js/app.js
+++ demo/js/app.js
@@ -23,7 +23,7 @@ particlesJS('particles-js',
         }
       },
       "color": {
-        "value": "#e91313"
+        "value": "#007ff8"
       },
       "shape": {
         "type": "circle",
@@ -63,7 +63,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#e91313",
+        "color": "#007ff8",
         "opacity": 0.4,
         "width": 1
       },
